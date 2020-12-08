49f09ab2df21b1993271802a1a110a6a86ed7ece
jimmycliff obonyo <cliffjimmy27@gmail.com>
2020-12-08 22:22:10 +0300

    Initial commit.

---
+++ rollup.config.js
@@ -0,0 +1,21 @@
+import resolve from 'rollup-plugin-node-resolve'
+import babel from 'rollup-plugin-babel'
+import commonjs from 'rollup-plugin-commonjs'
+import pkg from './package.json'
+
+export default {
+  input: 'src/index.js',
+  output: {
+    file: pkg.main,
+    format: 'es',
+    exports: 'auto'
+  },
+  external: ['react'],
+  plugins: [
+    babel({
+      exclude: 'node_modules/**'
+    }),
+    resolve(),
+    commonjs()
+  ]
+}
