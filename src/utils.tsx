72dd73950acf6a2c93dd01ff854488c2178951d1
jimmycliff obonyo <cliffjimmy27@gmail.com>
2020-12-11 00:21:14 +0300

    rollup - typescript configs

--- src/utils.tsx
+++ src/utils.tsx
@@ -1,13 +1,14 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from "react";
 
-const cachedScripts = []
+const cachedScripts = [];
 
-export function useScript (src) {
+export function useScript(src: string) {
+
   // Keeping track of script loaded and error state
   const [state, setState] = useState({
     loaded: false,
     error: false
-  })
+  });
 
   useEffect(
     () => {
@@ -17,50 +18,42 @@ export function useScript (src) {
         setState({
           loaded: true,
           error: false
-        })
+        });
       } else {
-        cachedScripts.push(src)
-
+        cachedScripts.push(src);
         // Create script
-        const script = document.createElement('script')
-        script.src = src
-        script.async = true
-
+        const script = document.createElement("script");
+        script.src = src;
+        script.async = true;
         // Script event listener callbacks for load and error
         const onScriptLoad = () => {
           setState({
             loaded: true,
             error: false
-          })
-        }
-
+          });
+        };
         const onScriptError = () => {
           // Remove from cachedScripts we can try loading again
-          const index = cachedScripts.indexOf(src)
-          if (index >= 0) cachedScripts.splice(index, 1)
-          script.remove()
-
+          const index = cachedScripts.indexOf(src);
+          if (index >= 0) cachedScripts.splice(index, 1);
+          script.remove();
           setState({
             loaded: true,
             error: true
-          })
-        }
-
-        script.addEventListener('load', onScriptLoad)
-        script.addEventListener('error', onScriptError)
-
+          });
+        };
+        script.addEventListener("load", onScriptLoad);
+        script.addEventListener("error", onScriptError);
         // Add script to document body
-        document.body.appendChild(script)
-
+        document.body.appendChild(script);
         // Remove event listeners on cleanup
         return () => {
-          script.removeEventListener('load', onScriptLoad)
-          script.removeEventListener('error', onScriptError)
-        }
+          script.removeEventListener("load", onScriptLoad);
+          script.removeEventListener("error", onScriptError);
+        };
       }
     },
     [src] // Only re-run effect if script src changes
-  )
-
-  return [state.loaded, state.error]
+  );
+  return [state.loaded, state.error];
 }
